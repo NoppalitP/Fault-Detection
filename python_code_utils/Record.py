9baf7366892ccfe1eabbcecd09f8d515cd3a08a5
NoppalitPosritat <155846151+NoppalitP@users.noreply.github.com>
2025-08-13 14:18:23 +0700

    a

--- python_code_utils/Record.py
+++ python_code_utils/Record.py
@@ -15,7 +15,7 @@ import sys
 
 # === CONFIGURATION ==========================================================
 # --- Serial Port Settings ---
-SERIAL_PORT = 'COM3'  # ตั้งค่าพอร์ตอนุกรมที่ต้องการ
+SERIAL_PORT = 'COM5'  # ตั้งค่าพอร์ตอนุกรมที่ต้องการ
 BAUD_RATE = 500000     # ตั้งค่า Baud Rate ให้ตรงกับอุปกรณ์
 
 # --- Audio Settings ---
@@ -27,13 +27,13 @@ SAMPLE_WIDTH = 2       # 2 bytes per sample (for 16-bit audio)
 BLOCK_SIZE = 512       # จำนวน sample ที่อ่านในแต่ละครั้ง
 
 # --- File Saving Settings ---
-WINDOW_DURATION = 2 # ความยาวของเสียงแต่ละไฟล์ (วินาที)
-STEP_DURATION = 1.0    # ความถี่ในการบันทึกไฟล์ใหม่ (วินาที)
+WINDOW_DURATION = 30 # ความยาวของเสียงแต่ละไฟล์ (วินาที)
+STEP_DURATION = 30.0    # ความถี่ในการบันทึกไฟล์ใหม่ (วินาที)
 
 #============================================================================
 
-OUTPUT_DIRECTORY = r"record\normal"
-NAME_COMPONENT= "environment"  # ชื่อของ component ที่บันทึก
+OUTPUT_DIRECTORY = r"record"
+NAME_COMPONENT= "envir_lab"  # ชื่อของ component ที่บันทึก
 
 
 # === DERIVED CONSTANTS (DO NOT CHANGE) ======================================
